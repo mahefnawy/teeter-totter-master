b6127bf444461ba7168c4b7e630742bfc669763e
Mohamed Hassan <mohamed.hassan@osf-commerce.com>
2019-12-19 17:34:17 +0300

    teeter-totter-master

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="root">
+        <Header/>
+        <GameArea/>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header'
+    import GameArea from './components/GameArea'
+
+    export default {
+        name: 'app',
+        components: {
+            Header,
+            GameArea
+        }
+    }
+</script>
+
+<style lang="scss">
+    .root{
+        overflow: hidden;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    body {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPause, faPlay);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
+
+new Vue({
+    store,
+    render: h => h(App)
+}).$mount('#app');
